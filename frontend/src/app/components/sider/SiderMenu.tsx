b2e834b3aae2474a83fe40198db3b59fe69cb6c2
khahnm04 <khanhnguyenminh1811@gmail.com>
2025-10-20 19:36:47 +0700

    fix (frontend): update code

--- frontend/src/app/components/sider/SiderMenu.tsx
+++ frontend/src/app/components/sider/SiderMenu.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 import { IconType } from "react-icons";
 
 export const SiderMenu = (props: {
-  menuList: Array<{  // ← Khai báo trực tiếp
+  menuList: Array<{
     name: string;
     link: string;
     icon: IconType;
@@ -15,29 +15,31 @@ export const SiderMenu = (props: {
 
   return (
     <>
-      <ul className="list-none p-0 my-[11px] mx-0">
-        {menuList.map((item, index) => (
-          <li key={index} className="">
-            <Link
-              href={item.link}
-              onClick={() => setActiveMenu(item.name)}
-              className={`flex items-center gap-[10px] py-[12px] px-[16px] ml-[24px] mr-[10px] rounded-[6px] font-[600] text-[14px] relative 
+      {menuList && (
+        <ul className="list-none p-0 my-[11px] mx-0">
+          {menuList.map((item, index) => (
+            <li key={index} className="">
+              <Link
+                href={item.link}
+                onClick={() => setActiveMenu(item.name)}
+                className={`flex items-center gap-[10px] py-[12px] px-[16px] ml-[24px] mr-[10px] rounded-[6px] font-[600] text-[14px] relative 
                   ${activeMenu === item.name
-                  ? 'bg-[var(--color-primary)] before:absolute before:left-[-24px] before:top-0 before:h-full before:w-[4.5px] before:rounded-[0_4px_4px_0] before:bg-[var(--color-primary)] before:content-[\'\']'
-                  : ''
-                } 
+                    ? 'bg-[var(--color-primary)] before:absolute before:left-[-24px] before:top-0 before:h-full before:w-[4.5px] before:rounded-[0_4px_4px_0] before:bg-[var(--color-primary)] before:content-[\'\']'
+                    : ''
+                  } 
                   ${activeMenu === item.name
-                  ? 'text-white'
-                  : item.isLogout
-                    ? 'text-[#F93C65]'
-                    : 'text-[var(--color-text)]'
-                }`}
-            >
-              <item.icon className="text-[16px]" /> {item.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
+                    ? 'text-white'
+                    : item.isLogout
+                      ? 'text-[#F93C65]'
+                      : 'text-[var(--color-text)]'
+                  }`}
+              >
+                <item.icon className="text-[16px]" /> {item.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
     </>
   )
 }
